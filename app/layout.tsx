80daf5ccbc452152cc898b709e336e2c57a0316c
Brandon Stone <42560473+KidLiberty@users.noreply.github.com>
2025-02-18 12:49:07 -0500

    Font fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css'
+import { inter } from './ui/fonts'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      {/* By adding the font to the body element, the font will be applied across the application */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
