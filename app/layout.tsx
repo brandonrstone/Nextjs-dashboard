d01fc70d250ef555d94ca48eb62ec85e1f63c2b8
Brandon Stone <brandonrstone@gmail.com>
2025-03-29 11:33:41 -0400

    Added more meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import Head from 'next/head'
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts'
 
@@ -12,6 +13,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
+      <Head>
+        <meta name='author' content='Brandon Stone' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='robots' content='index, follow' />
+        <link rel='icon' href='/favicon.svg' />
+      </Head>
       {/* By adding the font to the body element, the font will be applied across the application */}
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
